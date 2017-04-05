1517f8bb6681632f758d7498f9c91fd484b56a12
Arbauman <arbauman207@gmail.com>
2017-04-04 21:30:56 -0400

    Added .gitlab-ci.yml

--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
-import store from './store/store';
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App },
-  store
+  components: { App }
 });
